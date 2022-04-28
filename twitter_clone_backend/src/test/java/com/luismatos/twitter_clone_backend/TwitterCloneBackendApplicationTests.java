30cd27f69c303307d6adaebd2bad205bdae67b58
luis <lmatosgarcia2@gmail.com>
2022-04-28 02:19:41 -0400

    Add Spring Boot Backend Project

---
+++ twitter_clone_backend/src/test/java/com/luismatos/twitter_clone_backend/TwitterCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismatos.twitter_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
